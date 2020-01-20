451cd5333180fbfec1991c00648f3e842c017bbf
duzidavid <david.duzi@teliacompany.com>
2020-01-20 13:14:37 +0100

    adding OneToMany/ManyToOne bidirectional JPA

--- src/main/java/cz/duzi/recipeapp/domain/Recipe.java
+++ src/main/java/cz/duzi/recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package cz.duzi.recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,9 @@ public class Recipe {
     private String directions;
     // TODO: 20.01.2020
     //private Difficulty difficulty;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     @Lob
     private Byte[] image;
 
@@ -102,5 +106,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
