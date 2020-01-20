7950cb7026a9c1e2a132cc952a8963189b25db1b
duzidavid <david.duzi@teliacompany.com>
2020-01-20 15:37:16 +0100

    Spring data JPA Query(147)

--- src/main/java/cz/duzi/recipeapp/controllers/IndexController.java
+++ src/main/java/cz/duzi/recipeapp/controllers/IndexController.java
@@ -1,13 +1,33 @@
-package cz.duzi.recipe.controllers;
+package cz.duzi.recipeapp.controllers;
 
+import cz.duzi.recipeapp.domain.Category;
+import cz.duzi.recipeapp.domain.UnitOfMeasure;
+import cz.duzi.recipeapp.repositories.CategoryRepository;
+import cz.duzi.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 
--- src/main/java/cz/duzi/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/cz/duzi/recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package cz.duzi.recipeapp.repositories;
 import cz.duzi.recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
